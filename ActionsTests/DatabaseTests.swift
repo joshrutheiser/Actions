328fb540b21c4f9583b7ee2819f8884e5d2782de
Josh <josh.ruth@gmail.com>
2022-12-31 01:29:21 -0800

    added test for removing objects when they no longer match the query

--- ActionsTests/DatabaseTests.swift
+++ ActionsTests/DatabaseTests.swift
@@ -147,7 +147,44 @@ class DatabaseTests: XCTestCase {
         waitForExpectations(timeout: 5, handler: nil)
     }
 
-    //MARK: - Create multiple actions
+    //MARK: - Remove from listener
+    
+    func testRemoveFromListener() throws {
+        let exp = expectation(description: #function)
+        
+        var action = Action(userId, "2")
+        let _ = try! databaseWriter.create(as: Action.self, Action(userId, "1"))
+        let id2 = try! databaseWriter.create(as: Action.self, action)
+        let _ = try! databaseWriter.create(as: Action.self, Action(userId, "3"))
+        
+        databaseWriter.execute() { [self] in
+            
+            let query = QueryBuilder(Action.self, rootPath: testPath)
+                .user(userId)
+                .build()
+            
+            var count = 0
+            databaseReader.listenDocuments(query, as: Action.self) { results in
+                count += 1
+                if count == 2 {
+                    XCTAssertEqual(results.count, 1)
+                    XCTAssertEqual(results.first!.change, .Remove)
+                    XCTAssertEqual(results.first!.object.id, id2)
+                    exp.fulfill()
+                }
+
+            }
+            
+            action.id = id2
+            action.userId = ""
+            try! databaseWriter.update(as: Action.self, action)
+            databaseWriter.execute()
+        }
+        
+        waitForExpectations(timeout: 5, handler: nil)
+    }
+    
+    //MARK: Create multiple actions
     
     func testCreateMultipleActions() throws {
         let exp = expectation(description: #function)
@@ -172,6 +209,41 @@ class DatabaseTests: XCTestCase {
                 exp.fulfill()
             }
             
+        }
+        
+        waitForExpectations(timeout: 5, handler: nil)
+    }
+    
+    //MARK: - Remove from get docs
+    
+    func testRemoveFromGetDocs() throws {
+        let exp = expectation(description: #function)
+        
+        var action = Action(userId, "2")
+        let id1 = try! databaseWriter.create(as: Action.self, Action(userId, "1"))
+        let id2 = try! databaseWriter.create(as: Action.self, action)
+        let id3 = try! databaseWriter.create(as: Action.self, Action(userId, "3"))
+        
+        databaseWriter.execute() { [self] in
+            action.id = id2
+            action.userId = ""
+            try! databaseWriter.update(as: Action.self, action)
+            databaseWriter.execute() { [self] in
+                
+                let query = QueryBuilder(Action.self, rootPath: testPath)
+                    .user(userId)
+                    .build()
+                
+                databaseReader.getDocuments(query, as: Action.self) { [self] results in
+                    XCTAssertEqual(results.count, 2)
+                    for result in results {
+                        XCTAssertEqual(result.change, .Update)
+                        XCTAssert(Set([id1, id3]).contains(result.object.id))
+                        XCTAssertEqual(result.object.userId, userId)
+                    }
+                    exp.fulfill()
+                }
+            }
         }
         
         waitForExpectations(timeout: 5, handler: nil)
